37d9dc34b8172faa97d739bfcd27b5d6fa03ae8d
Sher Chowdhury <sher.chowdhury@ibm.com>
2019-09-14 23:03:12 +0100

    wip

--- members/members.go
+++ members/members.go
@@ -1,7 +1,7 @@
 /*
 Everything from the root of this repo downwards, is called a 'module'. A module
-can contain one or more packages. A 'package' is essentially a folder of golang code
-in the module. The root folder by design houses 'package main'. Any other child folder
+can contain one or more packages. In a module, a 'package' is essentially just a folder
+that house one or more *.go files. The root folder by design houses 'package main'. Any other child folder
 such as the 'members' folder is referred to as packages.
 
 child Packages must be named after the name of the folder that houses then.
